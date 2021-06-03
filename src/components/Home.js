a5d95bae06b5d16b48de46a6b27d15816cf3388d
anitkumarsingh <anikumarsingh99@gmail.com>
2021-06-03 19:58:48 +0530

    home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,15 +16,35 @@ const Home = () => {
   console.log('home ran', homes);
   homes = homeData.map((i, index) => {
     return (
-      <div data-testid="homes" key={index}>
-        <div data-testid="home-title">{i.title}</div>
-        <img data-testid="img" alt="home" src={i.image} />
-        <div data-testid="home-loc">{i.location}</div>
-        <div data-testid="price">{i.price}</div>
+      <div
+        data-testid="homes"
+        key={index}
+        className="col-6 col-md-6 col-lg-4 col-xl-3 mb-3"
+      >
+        <div className="card w-100">
+          <img
+            data-testid="img"
+            alt="home"
+            src={i.image}
+            className="card-img-top"
+          />
+          <div className="card-body">
+            <div data-testid="home-title" className="card-title h5">
+              {i.title}
+            </div>
+            <div data-testid="home-loc">{i.location}</div>
+            <div data-testid="price">{i.price}/night</div>
+          </div>
+        </div>
       </div>
     );
   });
-  return <div>{homes}</div>;
+  return (
+    <div className="container m-2">
+      <h1>Homes</h1>
+      <div className="row">{homes}</div>
+    </div>
+  );
 };
 
 export default Home;
